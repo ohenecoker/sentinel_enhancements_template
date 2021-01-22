152f13ba99ca2855130bf0599b47b7fbb90f4d1f
ohenecoker <coker.christian@gmail.com>
2021-01-22 11:21:05 -0500

    clean template

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,9 @@
 <script>
-  import DesktopMenu from "./menu/desktop_menu.svelte";
-  import MobileMenu from "./menu/mobile_menu.svelte";
-
   let x;
 </script>
 
 <svelte:window bind:innerWidth={x} />
-<main>
-  {#if x > 700}
-    <div>
-      <DesktopMenu />
-    </div>
-  {/if}
-  {#if x < 700}
-    <div>
-      <MobileMenu />
-    </div>
-  {/if}
-</main>
+<main />
 
 <style>
   main {
